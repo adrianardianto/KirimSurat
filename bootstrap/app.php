9304bf69b02bbec8c563ffc7e6a86c063c937422
Adrian Ardianto <adrianardianto10@gmail.com>
2026-02-01 17:37:22 +0700

    feat(config): configure trusted proxies and force https scheme for ngrok

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\GzipResponse::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
